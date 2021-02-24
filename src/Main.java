7241ff10d304d9b3f55c95e4a1fec3852bf99d58
diegocb81 <diegocb81@gmail.com>
2021-02-24 18:14:10 -0300

    Finalizando o projeto

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Random meurnd = new Random();
 
+        int indice = meurnd.nextInt(args.length);
+        System.out.println("Vencedor: " + args[indice]);
 
     }
 
